88b50eff71c8fe328cf18b60d3f3d5995feac753
HM-Ameni <hma.ameni@gmail.com>
2022-10-25 03:05:18 +0100

    Initial comment

---
+++ src/main/java/com/hma/katabook/KataBookApplication.java
@@ -0,0 +1,22 @@
+package com.hma.katabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KataBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataBookApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hedddddddddddddllo %s!", name);
+    }
+}
---
+++ src/test/java/com/hma/katabook/KataBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hma.katabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
